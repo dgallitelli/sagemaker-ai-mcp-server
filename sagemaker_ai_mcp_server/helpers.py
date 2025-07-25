737f4fcc775ac0c369bd2bf654e44c86ed854963
Bojan Jakimovski <bokicasheks@yahoo.com>
2025-07-25 23:45:49 +0200

    Adding helper functions for list user profiles and spaces

--- sagemaker_ai_mcp_server/helpers.py
+++ sagemaker_ai_mcp_server/helpers.py
@@ -595,3 +595,27 @@ async def create_presigned_domain_url(
         ExpirationSeconds=expiration_seconds,
     )
     return response.get('AuthorizedUrl', '')
+
+
+async def list_user_profiles() -> List[Dict[str, Any]]:
+    """List all user profiles in a SageMaker Domain.
+
+    Returns:
+        List[Dict[str, Any]]: A list of user profiles in the SageMaker Domain.
+    """
+    client = get_sagemaker_client()
+    logger.info('Listing SageMaker User Profiles...')
+    response = client.list_user_profiles()
+    return response.get('UserProfiles', [])
+
+
+async def list_spaces() -> List[Dict[str, Any]]:
+    """List all SageMaker Spaces.
+
+    Returns:
+        List[Dict[str, Any]]: A list of SageMaker Spaces.
+    """
+    client = get_sagemaker_client()
+    logger.info('Listing SageMaker Spaces...')
+    response = client.list_spaces()
+    return response.get('Spaces', [])
